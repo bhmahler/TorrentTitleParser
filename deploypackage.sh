286c4385482bcf5ddccb811d639190351899e5e3
Brian Mahler <bhmahler@gmail.com>
2019-08-20 15:12:54 -0400

    Updated deploy package script

--- deploypackage.sh
+++ deploypackage.sh
@@ -5,7 +5,7 @@ echo "Found version $version for publish, checking nuget.org..."
 lower=$(echo "$file" | tr '[:upper:]' '[:lower:]')
 url="https://globalcdn.nuget.org/packages/$lower"
 status=$(curl --write-out %{http_code} --silent --output /dev/null $url);
-if [ "$status" == "200" ]; then
+if [ $status=200 ]; then
 	echo "Version $version already found, skipping publish"
 else 
 	echo "Version $version not found, publishing..."
