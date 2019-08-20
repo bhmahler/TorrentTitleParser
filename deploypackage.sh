ded0093c4d1842e0427e3e603953a5c1f0f104aa
Brian Mahler <bhmahler@gmail.com>
2019-08-20 15:08:30 -0400

    Updated deploy package script

--- deploypackage.sh
+++ deploypackage.sh
@@ -1,17 +1,13 @@
 cd TorrentTitleParser/bin/Release/
 file="$(find ./ -type f -name *.nupkg -printf "%f")"
-echo $file
 version=$(echo $file | grep -o -E '[0-9]\.[0-9]\.[0-9]')
 echo "Found version $version for publish, checking nuget.org..."
 lower=$(echo "$file" | tr '[:upper:]' '[:lower:]')
 url="https://globalcdn.nuget.org/packages/$lower"
-echo $url
-status=$(curl --write-out %{http_code} --silent --output /dev/null $url)
-echo $status
+status=$(curl --write-out %{http_code} --silent --output /dev/null $url);
 if [ "$status" == "200" ]; then
 	echo "Version $version already found, skipping publish"
-	return 0;
 else 
 	echo "Version $version not found, publishing..."
-	dotnet nuget push $file -k $1 -s $2 
+	dotnet nuget push $file -k $1 -s $2
 fi
