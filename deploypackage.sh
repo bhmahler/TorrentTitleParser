9028feb9258887488f62a9fbd069b3edef49a6bd
Brian Mahler <bhmahler@gmail.com>
2019-08-20 13:26:09 -0400

    Added a deploy script to check if version already exists

---
+++ deploypackage.sh
@@ -0,0 +1,13 @@
+cd TorrentTitleParser/bin/Release/
+file="$(find -type f printf %f\\n -name *.nupkg)"
+echo $file
+version=$(echo $file | grep -o -E '[0-9]\.[0-9]\.[0-9]')
+echo $version
+status=$(curl -o /dev/null -Isw '%{http_code}\n' https://www.nuget.org/api/v2/packages/TorrentTitleParser/$version)
+if $status=200; then
+	echo "Version $version already found, skipping publish"
+	return 0;
+else 
+	echo "Version $version not found, publishing..."
+	dotnet nuget push $file -k $1 -s $2 
+fi
